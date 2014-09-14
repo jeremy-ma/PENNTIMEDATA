c19908658203bc99b67f5893ada3a44b62b3832f
jeremy-ma <jeremy_ma@outlook.com>
2014-09-14 16:01:12 +1000

    so tired

--- timeTabler/test.js
+++ timeTabler/test.js
@@ -137,6 +137,20 @@ function Class(coursecode, courseInfoString){
 		this.secArray.push(secObj);
 	}
 
+	//Arrange sections into jagged array based on type
+	this.arrangedSec = new Array();
+	for (i=0;i<this.secTypes.length;i++){
+		singleType = new Array();
+		for (j=0;j<this.secArray.length;j++){
+			if (this.secArray[j].getType == this.secTypes[i]) {
+				singleType.push(this.secArray[j]);
+			}
+		}
+		this.arrangedSec.push(singleType);
+	}
+
+
+
 }
 
 Class.prototype.getCode = function (){
@@ -149,6 +163,31 @@ Class.prototype.getSecTypes = function(){
 
 Class.prototype.getCourseName = function() {
 	return this.courseName;
+}
+
+Class.prototype.getPossibleSchedules = function() {
+	possibleSchedules = new Array();
+
+	if(this.secTypes.length == 1){
+		//if there's only one section to register for
+		for (t=0;t<this.arrangedSec[0];t++){
+			possibleSchedules.push(this.arrangedSec[0][t].getSched());
+		}
+	} else if (this.arrangedSec.length == 2) {
+		for (t=0;t<this.arrangedSec[0].length;t++){ 
+			for (r=0;r<this.arrangedSec[1].length;r++){
+				classSched = new Schedule();
+				sched1 = new Schedule(this.arrangedSec[0][t].getSched(),this.ccode)
+				sched2 = new Schedule(this.arrangedSec[1][r].getSched(),this.ccode)
+				classSched.add(sched1);
+				classSched.add(sched2);
+				possibleSchedules.push(classSched);
+			}
+		}
+	} else {
+		console.log("no too tired");
+	}
+
 }
 
 
@@ -179,6 +218,7 @@ function get_sub_data(crscodes,filename){
 
 function Schedule(section,courseCode){
 	
+
 	this.timeTable = new Array();
 	for (k=0; k<5;k++){
 		day = new Array();
@@ -188,16 +228,17 @@ function Schedule(section,courseCode){
 		this.timeTable.push(day);
 	}
 
-	days = section.getDays();
-	startTime = section.getStart();
-	endTime = section.getEnd();
+	if (section != undefined && courseCode != undefined){
+		days = section.getDays();
+		startTime = section.getStart();
+		endTime = section.getEnd();
 
-	for (b=0;b<days.length;b++){
-		for (v=startTime;v<endTime;v++){
-			section[b][v] = courseCode + '_' + section.getType;
+		for (b=0;b<days.length;b++){
+			for (v=startTime;v<endTime;v++){
+				section[b][v] = courseCode + '_' + section.getType;
+			}
 		}
 	}
-	
 }
 
 Schedule.prototype.getSchedule = function() {
@@ -210,13 +251,43 @@ Schedule.prototype.add = function(otherSched) {
 	other = otherSched.getSchedule();
 	cur = this.getSchedule();
 
+
 	for (m=0;m<5;m++){
 		for(n=0;n<48;n++){
 			cur[m][n]=cur[m][n] + other[m][n];
 		}
 	}
-
 	return cur;
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+///The algorithm!
+
+classList = get_sub_data('CIS191 CIS380 ESE304 ACCT101', 'finaldata.txt');
+
+var subjectScheds = new Array();
+
+var sub1 = classList[0].getPossibleSchedules();
+var sub2 = classList[1].getPossibleSchedules();
+var sub3 = classList[2].getPossibleSchedules();
+var sub4 = classList[3].getPossibleSchedules();
+
+for (i=0;i<sub1.length;i++){
+	for (j=0;j<sub1.length;j++){
+		for (k=0;k<sub1.length;k++){
+			for (l=0;l<sub1.length;l++){
+				new sched = new Schedule();
+				sched.add(sub1[i]);
+				sched.add(sub1[j]);
+				sched.add(sub1[k]);
+				sched.add(sub1[l]);
+				subjectScheds.push(sched);
+			}
+		}
+	}
 }
 
 
@@ -236,10 +307,6 @@ Schedule.prototype.add = function(otherSched) {
 
 
 
-
-
-
-array = get_sub_data('CIS191 CIS380 ESE304 ACCT101', 'finaldata.txt');
 
 for (i=0; i<array.length;i++) {
 
